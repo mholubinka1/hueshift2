fb7abdb24d59aba833561f5c1c4fafa2852c0cdd
mholubinka <meholubinka@gmail.com>
2021-04-19 18:49:13 +0100

    add memory state tolerance to async transitions to prevent lights being set to manual erroneously

--- HueShift2/HueShift2/Control/LightControlPair.cs
+++ HueShift2/HueShift2/Control/LightControlPair.cs
@@ -156,7 +156,9 @@ namespace HueShift2.Control
             if (command.TransitionTime != null)
             {
                 this.PowerState = LightPowerState.Transitioning;
-                this.Transition = new Transition(currentTime, (TimeSpan)command.TransitionTime);
+                var asyncTolerance = 5;
+                this.Transition = new Transition(currentTime, 
+                    ((TimeSpan)command.TransitionTime).Add(new TimeSpan(0,0, asyncTolerance)));
             }
             else
             {
