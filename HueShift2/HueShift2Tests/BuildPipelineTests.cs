5522d2f7419a11e72ce87db21349ecdc5ae6e318
mholubinka <meholubinka@gmail.com>
2021-05-07 09:06:07 +0100

    build pipeline tests

---
+++ HueShift2/HueShift2Tests/BuildPipelineTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HueShift2.UnitTests
+{   
+    [TestFixture]
+    public class BuildPipelineTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void BuildPipeline_TestAlwaysFails()
+        {
+            var @false = false;
+            Assert.That(@false == true);
+        }
+
+        [Test]
+        public void BuildPipeline_TestAlwaysPasses()
+        {
+            var @true = true;
+            Assert.That(@true == true);
+        }
+    }
+}
