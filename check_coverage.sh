2679ded7ae844419802cff8f1a175d82faa6704e
Matt Holubinka <meholubinka@gmail.com>
2022-02-09 18:31:51 +0000

    refactored pipeline

--- check_coverage.sh
+++ check_coverage.sh
@@ -12,12 +12,11 @@ echo "Target coverage value = " $TARGET_COVERAGE
 CURRENT_COVERAGE=`curl -s "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/jobs?private_token=${RUNNER_ACCESS_TOKEN}" | jq --arg JOB_NAME "$JOB_NAME" '.[] | select(.name==$JOB_NAME) | .coverage'`
 echo "Pipeline " ${CI_PIPELINE_ID} " coverage value = " $CURRENT_COVERAGE
 
-
-if [ "$CURRENT_COVERAGE" -ge "$TARGET_COVERAGE"]
+if (( $(echo "$TARGET_COVERAGE > $CURRENT_COVERAGE" | bc -l) ))
 then
+  echo "Coverage decreased from " ${TARGET_COVERAGE} " to " ${CURRENT_COVERAGE} " . Build Failed."
+  exit 1
+else
   echo "Build Succeeded."
   exit 0
-else
-  echo "Coverage decreased from ${TARGET_COVERAGE} to ${CURRENT_COVERAGE}. Build Failed."
-  exit 1
 fi
